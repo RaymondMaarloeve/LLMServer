f2a0da77a6e81367110b1fb70947133df3c2ee2d
Paweł Reich <37516105+Gitmanik@users.noreply.github.com>
2025-05-09 14:16:42 +0200

    Update hook-llama_cpp.py

--- hooks/hook-llama_cpp.py
+++ hooks/hook-llama_cpp.py
@@ -4,4 +4,4 @@ from PyInstaller.utils.hooks import collect_dynamic_libs
 
 # Automatically collect all shared libraries
 binaries = collect_dynamic_libs('llama_cpp')
-print(f"🚀 Hook executed at compile time: {binaries}")
+print(f"hook-llama_cpp: Hook executed at compile time: {binaries}")
